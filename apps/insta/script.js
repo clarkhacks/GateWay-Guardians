0a4c4d3c35ef33c9bbe5d37134100720e853e62c
Clark Weckmann <clarkhacks@users.noreply.github.com>
2020-10-30 12:12:16 -0500

    Add files via upload

---
+++ apps/insta/script.js
@@ -0,0 +1,29 @@
+// Kumke
+getData("sgt__kumke", 60);
+// Corn
+getData("ilng_joshc", 151)
+//Davey
+getData("sergeant_seth", 0)
+//Hedger
+getData("ssghedgehog", 0)
+//Gentille
+getData("sfc_hulk", 0);
+//Schumacher
+getData("sgt_schu", 70);
+// Saenz
+getData("sfc_super_mario", 0)
+// Branz
+getData("sgt_branz", 2235)
+//womack
+getData("goguardorgohome", 3253)
+
+function getData(nco, oldCount){
+  $.getJSON("https://www.instagram.com/" + nco + "/?__a=1", function(result){
+	var user = result.graphql.user;
+	$("#tf-" + nco).append(user.edge_followed_by.count);
+	$("#f-" + nco).append(user.edge_followed_by.count - oldCount);
+	$("#photo-" + nco).attr("src", user.profile_pic_url);
+    	$("#name-" + nco).append(user.username);
+  });
+
+}
