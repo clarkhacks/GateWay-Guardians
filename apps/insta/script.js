debbeda27066e7c2c6b7cf08e76ed6d53b0e6fee
Clark Weckmann <clark@weckmann.me>
2021-01-14 09:43:54 -0600

    ratchet added

--- apps/insta/script.js
+++ apps/insta/script.js
@@ -1,34 +1,60 @@
 
 
-function getData(nco, oldCount){
-  $.getJSON("https://www.instagram.com/" + nco + "/?__a=1", function(result){
-	var user = result.graphql.user;
-	$("#tf-" + nco).append(user.edge_followed_by.count);
-	console.log(user.edge_follow_by.count);
-	$("#f-" + nco).append(user.edge_followed_by.count - oldCount);
-	$("#photo-" + nco).attr("src", user.profile_pic_url);
-    	$("#name-" + nco).append(user.username);
-  });
+// function getData(nco, oldCount){
+//   $.getJSON("https://www.instagram.com/" + nco + "/?__a=1", function(result){
+// 	var user = result.graphql.user;
+// 	var fC = user.edge_followed_by.count;
+// 	$("#tf-" + nco).append(fC);
+// 	$("#f-" + nco).append(fC - oldCount);
+// 	$("#photo-" + nco).attr("src", user.profile_pic_url);
+//     	$("#name-" + nco).append(user.username);
+//   });
 
-}
+// }
+
+
+
+
+function getData(nco, oldCount){
+	$.ajax({
+		url:"https://www.instagram.com/" + nco + "/?__a=1",
+		dataType: 'jsonp', 
+		success:function(json){
+			var user = result.graphql.user;
+			var fC = user.edge_followed_by.count;
+			$("#tf-" + nco).append(fC);
+			$("#f-" + nco).append(fC - oldCount);
+			$("#photo-" + nco).attr("src", user.profile_pic_url);
+				$("#name-" + nco).append(user.username);
+		},
+		error:function(){
+
+		}      
+	});
+  
+  }
+  
+
+
+
 
 // Kumke
 getData("sgt__kumke", 60);
 // Corn
-getData("ilng_joshc", 151)
+getData("ilng_joshc", 151);
 //Davey
-getData("sergeant_seth", 0)
+getData("sergeant_seth", 0);
 //Hedger
-getData("ssghedgehog", 0)
+getData("ssghedgehog", 0);
 //Gentille
 getData("sfc_hulk", 0);
 //Schumacher
 getData("sgt_schu", 70);
 // Saenz
-getData("sfc_super_mario", 0)
+getData("sfc_super_mario", 0);
 // Branz
-getData("sgt_branz", 2235)
+getData("sgt_branz", 2235);
 //womack
-getData("goguardorgohome", 3253)
+getData("goguardorgohome", 3253);
 //weckmann
-getData("spc_weckmann", 728)
+getData("spc_weckmann", 728);
